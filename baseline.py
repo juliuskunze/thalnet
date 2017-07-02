faf408e4ccf9d7f6fbeeeb6e822f99693148e888
Julius Kunze <juliuskunze@gmail.com>
2017-07-02 15:50:10 +0200

    Extracted placeholders and training code, added support for stacked RNNs

--- baseline.py
+++ baseline.py
@@ -1,30 +1,18 @@
-import itertools
-from pathlib import Path
-
-import numpy as np
 import tensorflow as tf
-from sets import Dataset
+from lazy import lazy
 
-from tools import timestamp, define_scope
+from tools import define_scope
 
 
-class GruModel:
-    def __init__(self,
-                 batch_size: int = 50,
-                 step_size: int = 28,
-                 input_size: int = 28,
-                 state_size: int = 10,
-                 output_size: int = 10):
-        self.batch_size = batch_size
-        self.step_size = step_size
-        self.input_size = input_size
-        self.state_size = state_size
-        self.output_size = output_size
+class RnnBaseline:
+    def __init__(self, data, target, dropout, num_hidden=10, num_layers=4):
+        self.num_layers = num_layers
+        self.num_hidden = num_hidden
+        self.dropout = dropout
 
-        self.input_batches
-        self.label_batch
-        self.rnn
-        self.logits
+        self.data = data
+        self.target = target
+
         self.prediction
         self.cross_entropy
         self.accuracy
@@ -33,54 +21,34 @@ class GruModel:
         self.test_summary
         self.weights_summary
 
-    @define_scope
-    def input_batches(self):
-        return [tf.placeholder(tf.float32, (self.batch_size, self.input_size), name=f'{step}') for step in
-                range(self.step_size)]
-
-    @define_scope
-    def label_batch(self):
-        return tf.placeholder(tf.float32, (self.batch_size, self.output_size), name='0')
-
-    @define_scope
-    def rnn(self):
-        gru = tf.contrib.rnn.GRUCell(self.state_size)
-        initial_state = tf.zeros([self.batch_size, self.state_size])
-        state = initial_state
-
-        for step_batch in self.input_batches:
-            # Output is equal to the new state for GRU cells, ignore it:
-            _, state = gru(step_batch, state)
-        return state
-
-    @define_scope
+    @lazy
     def logits(self):
-        return tf.contrib.layers.fully_connected(self.rnn, self.output_size, activation_fn=None)
+        network = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.GRUCell(self.num_hidden) for _ in range(self.num_layers)])
+        network = tf.contrib.rnn.DropoutWrapper(network, output_keep_prob=1 - self.dropout)
+        output, last_state = tf.nn.dynamic_rnn(network, self.data, dtype=tf.float32)
+
+        output = tf.transpose(output, [1, 0, 2])
+        last_output = tf.gather(output, int(output.shape[0]) - 1)
+
+        return tf.contrib.layers.fully_connected(last_output, num_outputs=int(self.target.shape[1]),
+                                                 activation_fn=None)
 
     @define_scope
     def prediction(self):
         return tf.nn.softmax(self.logits)
 
     @define_scope
-    def accuracy(self):
-        correct = tf.equal(tf.arg_max(self.logits, dimension=1), tf.arg_max(self.label_batch, dimension=1))
-        return tf.reduce_mean(tf.cast(correct, dtype=tf.float32))
+    def cross_entropy(self):
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.target))
 
     @define_scope
-    def cross_entropy(self):
-        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.label_batch))
+    def accuracy(self):
+        correct = tf.equal(tf.arg_max(self.logits, dimension=1), tf.arg_max(self.target, dimension=1))
+        return tf.reduce_mean(tf.cast(correct, dtype=tf.float32))
 
     @define_scope
     def optimize(self):
         return tf.train.RMSPropOptimizer(learning_rate=1e-3).minimize(self.cross_entropy)
-
-    @define_scope('train')
-    def train_summary(self):
-        return self.summary()
-
-    @define_scope('test')
-    def test_summary(self):
-        return self.summary()
 
     @define_scope('weights')
     def weights_summary(self):
@@ -93,35 +61,10 @@ class GruModel:
         test_accuracy_summary = tf.summary.scalar(f'accuracy', self.accuracy)
         return tf.summary.merge([test_cross_entropy_summary, test_accuracy_summary])
 
-    def train(self, train: Dataset, test: Dataset, batch_size: int = 50,
-              log_path: Path = Path.home() / '.tensorboard' / timestamp()):
-        session = tf.Session()
+    @define_scope('train')
+    def train_summary(self):
+        return self.summary()
 
-        session.run(tf.global_variables_initializer())
-
-        summary_writer = tf.summary.FileWriter(str(log_path), graph=tf.get_default_graph())
-
-        def feed_dict(batch: Dataset) -> dict:
-            step_batches = np.swapaxes(batch['data'], 0, 1)
-
-            return dict(
-                [(f'input_batches/{step}:0', step_batches[step]) for step in range(self.step_size)] +
-                [('label_batch/0:0', batch['target'])])
-
-        for batch_index in itertools.count():
-            _, train_cross_entropy, train_accuracy, train_summary = session.run(
-                [self.optimize, self.cross_entropy, self.accuracy, self.train_summary],
-                feed_dict(train.sample(batch_size)))
-
-            summary_writer.add_summary(train_summary, batch_index)
-
-            test_cross_entropy, test_accuracy, test_summary = session.run(
-                [self.cross_entropy, self.accuracy, self.test_summary], feed_dict(test.sample(batch_size)))
-
-            summary_writer.add_summary(test_summary, batch_index)
-
-            if batch_index % 100 == 0:
-                summary_writer.add_summary(session.run(self.weights_summary), batch_index)
-
-            print(
-                f'Batch {batch_index}: train accuracy {train_accuracy:.3f} (cross entropy {train_cross_entropy:.3f}), test accuracy {test_accuracy:.3f} (cross entropy {test_cross_entropy:.3f})')
+    @define_scope('test')
+    def test_summary(self):
+        return self.summary()
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import numpy as np
-from sets import Mnist
+import itertools
+from pathlib import Path
 
-from baseline import GruModel
+import numpy as np
+import tensorflow as tf
+from sets import Mnist, Dataset
+
+from baseline import RnnBaseline
+from tools import timestamp
 
 
 def plot(image: np.ndarray, label: str) -> None:
@@ -14,8 +19,46 @@ def plot(image: np.ndarray, label: str) -> None:
 # for image, label in train.sample(batch_size)[:1]:
 #    plot(image, label)
 
-baseline = GruModel()
+def main(batch_size: int = 50, log_path: Path = Path.home() / '.tensorboard' / timestamp()):
+    train, test = Mnist()
+    _, num_rows, row_size = train.data.shape
+    num_classes = train.target.shape[1]
+    data = tf.placeholder(tf.float32, [None, num_rows, row_size], name='data')
+    target = tf.placeholder(tf.float32, [None, num_classes], name='target')
+    dropout = tf.placeholder(tf.float32, name='dropout')
 
-train, test = Mnist()
+    model = RnnBaseline(data, target, dropout)
 
-baseline.train(train=train, test=test)
+    session = tf.Session()
+
+    session.run(tf.global_variables_initializer())
+
+    summary_writer = tf.summary.FileWriter(str(log_path), graph=tf.get_default_graph())
+
+    def feed_dict(batch: Dataset) -> dict:
+        return {data: batch.data, target: batch.target, dropout: 0}
+
+    for batch_index in itertools.count():
+        train_batch = train.sample(batch_size)
+
+        _, train_cross_entropy, train_accuracy, train_summary = session.run(
+            [model.optimize, model.cross_entropy, model.accuracy, model.train_summary],
+            feed_dict(train_batch))
+
+        summary_writer.add_summary(train_summary, batch_index)
+
+        test_batch = test.sample(batch_size)
+        test_cross_entropy, test_accuracy, test_summary = session.run(
+            [model.cross_entropy, model.accuracy, model.test_summary], feed_dict(test_batch))
+
+        summary_writer.add_summary(test_summary, batch_index)
+
+        if batch_index % 100 == 0:
+            summary_writer.add_summary(session.run(model.weights_summary), batch_index)
+
+        print(
+            f'Batch {batch_index}: train accuracy {train_accuracy:.3f} (cross entropy {train_cross_entropy:.3f}), test accuracy {test_accuracy:.3f} (cross entropy {test_cross_entropy:.3f})')
+
+
+if __name__ == '__main__':
+    main()
